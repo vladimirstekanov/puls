fd7e6faa90767faba15f4262ba2d4f5fb55d6a07
Vova <volodynarabote@yandex.ru>
2022-02-02 22:21:38 +0300

    js tab

--- src/js/script.js
+++ src/js/script.js
@@ -5,4 +5,34 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>'
       });
+
+    $('ul.catalog__tabs').each(function(i) {
+        var storage = localStorage.getItem('tab' + i);
+        if (storage) {
+          $(this).find('li').removeClass('catalog__tab_active').eq(storage).addClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq(storage).addClass('catalog__content_active');
+        }
+      });
+   
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+    .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+    .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    var ulIndex = $('ul.catalog__tabs').index($(this).parents('ul.catalog__tabs'));
+    localStorage.removeItem('tab' + ulIndex);
+    localStorage.setItem('tab' + ulIndex, $(this).index());
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
   });
